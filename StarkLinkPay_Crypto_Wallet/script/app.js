da99b48b3a2fe511c53c34720ad0100a1196f119
ayomide <sulaimanayomide005@gmail.com>
2025-05-26 06:49:18 +0100

    multipage

--- StarkLinkPay_Crypto_Wallet/script/app.js
+++ StarkLinkPay_Crypto_Wallet/script/app.js
@@ -7,6 +7,45 @@ window.addEventListener("load", () => {
     }, 2500)
 })
 
+const createWalletBtn = document.getElementById('createWallet')
+
+function showPage(pageId) {
+    const pages = document.querySelectorAll('.page')
+    pages.forEach(page => page.classList.remove('active'))
+
+    document.getElementById(pageId).classList.add('active')
+}
+
+createWalletBtn.addEventListener('click', showPage())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class QRCodeGenerator {
     constructor() {
